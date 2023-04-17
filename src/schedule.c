adb98fd0b22a2d7d047733d9869f98d1346ea44e
Henrik Karlsson <hakarlsson@mailbox.org>
2023-04-17 13:21:24 +0200

    Optimize scheduler

--- src/schedule.c
+++ src/schedule.c
@@ -33,45 +33,65 @@ void schedule_delete(uint64_t hartid, uint64_t begin, uint64_t end)
 
 struct proc *schedule_next()
 {
-	uint64_t hartid = csrr_mhartid();
-	uint64_t quantum;
+	uint64_t hartid;
+	uint64_t quantum, start_time, end_time;
 	struct proc *proc;
 	struct sched_entry entry;
+
+	// Get the hart ID.
+	hartid = csrr_mhartid();
+
 retry:
-	for (;;) {
-		quantum = (time_get() + NSLACK) / NTICK;
-		entry = schedule_get(hartid, quantum % NSLICE);
-		if (entry.pid == NONE_PID)
-			continue;
-		proc = proc_get(entry.pid);
-		if (proc->sleep > time_get())
-			continue;
-		if (proc_acquire(proc, PS_READY))
-			break;
-	}
-	proc_load_pmp(proc);
-	if (!csrr_pmpcfg0()) {
-		// Temporary fix. QEMU does not allow this to be zero.
-		proc_release(proc);
+	// Get the quantum.
+	quantum = (time_get() + NSLACK) / NTICK;
+
+	// Get the scheduled process
+	entry = schedule_get(hartid, quantum % NSLICE);
+	if (entry.pid == NONE_PID)
 		goto retry;
-	}
 
-	// Start time of a process
-	uint64_t start_time = quantum * NTICK;
-	// End time of a process
-	uint64_t end_time = start_time + entry.len * NTICK - NSLACK;
+	// Get the process
+	proc = proc_get(entry.pid);
 
+	// Calculate start and end time.
+	start_time = quantum * NTICK;
+	end_time = start_time + entry.len * NTICK - NSLACK;
+
+	// Check if process is sleeping.
+	if (proc->sleep > end_time)
+		goto retry;
+
+	// Set start_time to sleep if sleep is larger.
+	if (proc->sleep > start_time)
+		start_time = proc->sleep;
+
+	// Setup the PMP.
+	proc_load_pmp(proc);
+
+	// Temporary fix, QEMU does not allow this to be zero.
+	if (!csrr_pmpcfg0())
+		goto retry;
+
+	// Acquire the process.
+	if (!proc_acquire(proc, PS_READY))
+		goto retry;
+
+	// Wait until start_time
 	timeout_set(hartid, start_time);
-	while (!(csrr_mip() & (1 << 7))) {
+	while (!(csrr_mip() & (1 << 7)))
 		wfi();
-	}
+
+	// Set timeout
 	timeout_set(hartid, end_time);
+
 	return proc;
 }
 
 struct proc *schedule_yield(struct proc *proc)
 {
+	// Release currently held process.
 	proc_release(proc);
+	// Schedule the next process.
 	return schedule_next();
 }
 
