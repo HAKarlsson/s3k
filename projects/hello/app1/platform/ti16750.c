9c339a70e4f7b34ec501262ab95ac32bb86b67d5
Henrik Karlsson <hakarlsson@proton.me>
2026-05-03 19:32:46 +0000

    refactor: simplify uart register structure and update UART_REG_STRIDE checks

--- projects/hello/app1/platform/ti16750.c
+++ projects/hello/app1/platform/ti16750.c
@@ -2,57 +2,52 @@
 
 extern volatile int __uart_base[]; // UART base address
 
-#ifndef UART_REG_STRIDE
+#if UART_REG_STRIDE == 1
+typedef char uart_reg_t;
+#elif UART_REG_STRIDE == 4
+typedef int uart_reg_t;
+#else
 #error "UART_REG_STRIDE must be defined (1 or 4)."
 #endif
 
-#if (UART_REG_STRIDE != 1) && (UART_REG_STRIDE != 4)
-#error "Unsupported UART_REG_STRIDE. Use 1 or 4."
-#endif
-
-struct __attribute__((packed)) uart_reg_t {
-	volatile char val;
-	char __pad[UART_REG_STRIDE - 1];
-};
-
 struct __attribute__((packed)) uart_regs {
 	union {
-		struct uart_reg_t rbr;
-		struct uart_reg_t thr;
-		struct uart_reg_t dll;
+		uart_reg_t rbr;
+		uart_reg_t thr;
+		uart_reg_t dll;
 	};
 
 	union {
-		struct uart_reg_t ier;
-		struct uart_reg_t dlm;
+		uart_reg_t ier;
+		uart_reg_t dlm;
 	};
 
 	union {
-		struct uart_reg_t iir;
-		struct uart_reg_t fcr;
-		struct uart_reg_t efr;
+		uart_reg_t iir;
+		uart_reg_t fcr;
+		uart_reg_t efr;
 	};
 
-	struct uart_reg_t lcr;
+	uart_reg_t lcr;
 
 	union {
-		struct uart_reg_t mcr;
-		struct uart_reg_t xon1;
-	};
-
-	union {
-		struct uart_reg_t lsr;
-		struct uart_reg_t xon2;
+		uart_reg_t mcr;
+		uart_reg_t xon1;
 	};
 
 	union {
-		struct uart_reg_t msr;
-		struct uart_reg_t xoff1;
+		uart_reg_t lsr;
+		uart_reg_t xon2;
 	};
 
 	union {
-		struct uart_reg_t spr;
-		struct uart_reg_t xoff2;
+		uart_reg_t msr;
+		uart_reg_t xoff1;
+	};
+
+	union {
+		uart_reg_t spr;
+		uart_reg_t xoff2;
 	};
 };
 
@@ -60,9 +55,9 @@ int __uart_putc(char c, FILE *f)
 {
 	(void)f;
 	volatile struct uart_regs *regs = (struct uart_regs *)__uart_base;
-	while (!(regs->lsr.val & 0x20)) {
+	while (!(regs->lsr & 0x20)) {
 	}
-	regs->thr.val = (unsigned char)c;
+	regs->thr = (unsigned char)c;
 	return c;
 }
 
